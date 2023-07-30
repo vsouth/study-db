452091b4e25f66a6bb5e57cb4cdb00a09b34810a
vsouth <neppchela@gmail.com>
2023-07-31 01:53:57 +0300

    Changed structure of a project a bit, added more funcs to crud.py

--- study_db/cli.py
+++ study_db/cli.py
@@ -2,12 +2,12 @@ import sys, os
 
 sys.path.insert(0, os.path.dirname(__file__))
 
-from crud import session_scope
+import crud
 from models import Book, Genre, Author
 
 
 def add_author_by_hand():
-    with session_scope() as s:
+    with crud.session_scope() as s:
         name = input("Author name: ")
         biography = input("Author biography: ")
         author = Author(name=name, biography=biography)
@@ -15,22 +15,22 @@ def add_author_by_hand():
 
 
 def add_book_by_hand():
-    with session_scope() as s:
+    with crud.session_scope() as s:
         title = input("Book title: ")
-        author_name = input("Book author: ")
-        book = Book(title=title, author_name=author_name)
+        author_id = input("Book author: ")
+        book = Book(title=title, author_id=author_id)
         s.add(book)
 
 
 def add_genre_by_hand():
-    with session_scope() as s:
+    with crud.session_scope() as s:
         name = input("Genre name: ")
         description = input("Genre description: ")
         genre = Genre(name=name, description=description)
 
 
 def add_book_genres_by_hand():
-    with session_scope() as s:
+    with crud.session_scope() as s:
         all_genres = s.query(Genre).all()
         print(*all_genres, sep="\n")
         books = s.query(Book).filter(Book.genres == None).all()
@@ -54,14 +54,16 @@ def get_or_create(session, model, **kwargs):
         return instance
 
 
-def give_books_author_ids():
-    with session_scope() as s:
-        books = s.query(Book).filter(Book.author == None).all()
-        authors = s.query(Author).all()
-        for book in books:
-            author = get_or_create(s, Author, name=book.author_name)
-            book.author_id = author.id
+def get_all_genres():
+    with crud.session_scope() as s:
+        print(*s.query(Genre).all(), sep="\n")
 
 
-if __name__ == "__main__":
-    give_books_author_ids()
+def get_all_books():
+    with crud.session_scope() as s:
+        print(*s.query(Book).all(), sep="\n")
+
+
+def get_all_authors():
+    with crud.session_scope() as s:
+        print(*s.query(Author).all(), sep="\n")
